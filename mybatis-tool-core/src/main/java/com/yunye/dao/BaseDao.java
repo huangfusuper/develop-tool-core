28cefb4d0ca31e5a29200e95809d511e4b7bc250
huangfusuper <huangfusuper@163.com>
2020-03-19 20:14:03 +0800

    【编码】myBatis基类定义

---
+++ mybatis-tool-core/src/main/java/com/yunye/dao/BaseDao.java
@@ -0,0 +1,18 @@
+package com.yunye.dao;
+
+import com.yunye.help.SqlGenerateHelp;
+
+import java.util.Map;
+
+/**
+ * 持久层基础操作 基类
+ * @author huangfu
+ */
+public interface BaseDao {
+    /**
+     * 查询单条数据
+     * @param sqlGenerateHelp
+     * @return
+     */
+    Map<?,?> findOnBySqlGenerateHelp(SqlGenerateHelp sqlGenerateHelp);
+}
